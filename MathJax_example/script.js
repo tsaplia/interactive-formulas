768eb1df53c1c79dde35828dec235859146346ab
tsaplia <yunnakheifets@gmail.com>
2022-09-16 21:53:11 +0200

    added MathJax rendering

---
+++ MathJax_example/script.js
@@ -0,0 +1,21 @@
+const renderButton = document.getElementById("render-button");
+const inputField = document.getElementById("input-field");
+const outputField = document.getElementById("output-field");
+const statusField = document.getElementById("status");
+
+renderButton.addEventListener("click", () => {
+    outputField.innerHTML = "$$"+inputField.value+"$$";
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub,outputField], [addListeners]);
+});
+
+function addListeners(){
+
+}
+
+function getStructure(elem){
+    for(struct in structuresNames){
+        if(elem.className.includes(struct))
+            return structuresNames[struct];
+    }
+}
+
